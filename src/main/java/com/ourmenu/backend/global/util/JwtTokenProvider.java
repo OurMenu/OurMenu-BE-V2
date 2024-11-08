e3d6e9114baa9985e10f01dccd122bcb9d60cc59
You-Hyuk <cxz6411@naver.com>
2024-11-08 21:13:36 +0900

    fix: JWT Claim userId -> email 변경 및 메소드 추가, 수정

--- src/main/java/com/ourmenu/backend/global/util/JwtTokenProvider.java
+++ src/main/java/com/ourmenu/backend/global/util/JwtTokenProvider.java
@@ -1,93 +1,116 @@
 package com.ourmenu.backend.global.util;
 
 import com.ourmenu.backend.domain.user.application.CustomUserDetailsService;
+import com.ourmenu.backend.domain.user.dao.RedisRepository;
+import com.ourmenu.backend.domain.user.domain.RefreshToken;
+import com.ourmenu.backend.domain.user.dto.SignUpResponse;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
+import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
+import java.util.Optional;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class JwtTokenProvider {
 
     @Value("${jwt.secret}")
     private String secretKey;
 
-    @Value("${jwt.access.expiration}")
-    private long accessTokenValidityInMilliseconds;
 
-    @Value("${jwt.refresh.expiration}")
-    private long refreshTokenValidityInMilliseconds;
+    private Key key;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    private static final long ACCESS_TIME =  60 * 1000L;
+    private static final long REFRESH_TIME =  2 * 60 * 1000L;
+    public static final String ACCESS_TOKEN = "Access_Token";
+    public static final String REFRESH_TOKEN = "Refresh_Token";
 
     private final CustomUserDetailsService customUserDetailsService;
+    private final RedisRepository redisRepository;
 
-    // Access Token 생성
-    public String createAccessToken(Long userId) {
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
 
+    public String getHeaderToken(HttpServletRequest request, String type) {
+        return type.equals("Access") ? request.getHeader(ACCESS_TOKEN) :request.getHeader(REFRESH_TOKEN);
+    }
+
+    // 토큰 생성
+    public SignUpResponse createAllToken(String email) {
         Date now = new Date();
-        Date validity = new Date(now.getTime() + accessTokenValidityInMilliseconds);
+
+        String accessToken = createToken(email, "Access");
+        long accessTokenExpiredAt = now.getTime() + ACCESS_TIME;
+
+        String refreshToken = createToken(email, "Refresh");
+        long refreshTokenExpiredAt = now.getTime() + REFRESH_TIME;
+
+        return new SignUpResponse("Bearer", accessToken, refreshToken, accessTokenExpiredAt, refreshTokenExpiredAt);
+    }
+
+    public String createToken(String email, String type) {
+
+        Date date = new Date();
+
+        long time = type.equals("Access") ? ACCESS_TIME : REFRESH_TIME;
 
         return Jwts.builder()
-                .claim("userId", userId)
-                .setIssuedAt(now)
-                .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .setSubject(email)
+                .setExpiration(new Date(date.getTime() + time))
+                .setIssuedAt(date)
+                .signWith(key, signatureAlgorithm)
                 .compact();
+
     }
 
-    // Refresh Token 생성
-    public String createRefreshToken(Long userId) {
-        Date now = new Date();
-        Date validity = new Date(now.getTime() + refreshTokenValidityInMilliseconds);
-
-        return Jwts.builder()
-                .claim("userId", userId)
-                .setIssuedAt(now)
-                .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact();
-    }
-
-    // 토큰으로부터 인증 객체를 가져옴
-    public Authentication getAuthentication(String token) {
-        UserDetails userDetails = customUserDetailsService.loadUserByUsername(getUserId(token));
-        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
-    }
-
-    // 토큰에서 사용자 ID 추출
-    public String getUserId(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
-    }
-
-    // Request 헤더에서 토큰을 가져옴
-    public String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-        return null;
-    }
-
-    // 토큰 유효성 검증
-    public boolean validateToken(String token) {
+    public Boolean tokenValidation(String token) {
         try {
-            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
             return true;
-        } catch (Exception e) {
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
             return false;
         }
     }
 
-    // Refresh Token 만료 시간 반환
-    public long getRefreshTokenExpiration() {
-        return refreshTokenValidityInMilliseconds;
+    public Boolean refreshTokenValidation(String token) {
+        if(!tokenValidation(token)) return false;
+        Optional<RefreshToken> refreshToken = redisRepository.findByEmail(getEmailFromToken(token));
+        return refreshToken.isPresent() && token.equals(refreshToken.get().getRefreshToken());
+    }
+
+    public Authentication createAuthentication(String email) {
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public void setHeaderAccessToken(HttpServletResponse response, String accessToken) {
+        response.setHeader("Access_Token", accessToken);
+    }
+
+    public void setHeaderRefreshToken(HttpServletResponse response, String refreshToken) {
+        response.setHeader("Refresh_Token", refreshToken);
     }
 }
