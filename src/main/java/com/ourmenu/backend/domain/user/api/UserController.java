456be1e7c987c737cfdf4f29648887a2c03223a2
You-Hyuk <cxz6411@naver.com>
2024-11-10 20:25:00 +0900

    feat: SignUp, SignIn

---
+++ src/main/java/com/ourmenu/backend/domain/user/api/UserController.java
@@ -0,0 +1,32 @@
+package com.ourmenu.backend.domain.user.api;
+
+import com.ourmenu.backend.domain.user.application.UserService;
+import com.ourmenu.backend.domain.user.dto.SignInRequest;
+import com.ourmenu.backend.domain.user.dto.SignInResponse;
+import com.ourmenu.backend.domain.user.dto.SignUpRequest;
+import com.ourmenu.backend.global.util.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    private ResponseEntity<?> signUp(@RequestBody SignUpRequest signUpRequest){
+        String response = userService.signUp(signUpRequest);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/signin")
+    private ResponseEntity<?> signUp(@RequestBody SignInRequest request){
+        SignInResponse response = userService.signIn(request);
+        return ResponseEntity.ok(response);
+    }
+
+}
---
+++ src/main/java/com/ourmenu/backend/domain/user/application/UserService.java
@@ -0,0 +1,89 @@
+package com.ourmenu.backend.domain.user.application;
+
+import com.ourmenu.backend.domain.user.dao.RefreshTokenRepository;
+import com.ourmenu.backend.domain.user.dao.UserMealTimeRepository;
+import com.ourmenu.backend.domain.user.dao.UserRepository;
+import com.ourmenu.backend.domain.user.domain.RefreshToken;
+import com.ourmenu.backend.domain.user.domain.SignInType;
+import com.ourmenu.backend.domain.user.domain.User;
+import com.ourmenu.backend.domain.user.domain.UserMealTime;
+import com.ourmenu.backend.domain.user.dto.SignInRequest;
+import com.ourmenu.backend.domain.user.dto.SignInResponse;
+import com.ourmenu.backend.domain.user.dto.SignUpRequest;
+import com.ourmenu.backend.global.util.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserMealTimeRepository userMealTimeRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+
+
+    /**
+     *  회원가입 할 경우 User와 해당 userMealTime을 저장 및 Response 방환
+     * @param signUpRequest
+     * @return signUpResponse
+     */
+    public String signUp(SignUpRequest signUpRequest) {
+
+        if(userRepository.findByEmail(signUpRequest.getEmail()).isPresent()){
+            throw new RuntimeException("같은 이메일이 존재합니다");
+        }
+
+        // 패스워드 암호화
+        String encodedPassword = passwordEncoder.encode(signUpRequest.getPassword());
+
+        User user = User.builder()
+                .email(signUpRequest.getEmail())
+                .password(encodedPassword)
+                .signInType(SignInType.valueOf(signUpRequest.getSignInType()))
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        UserMealTime userMealTime = UserMealTime.builder()
+                .mealTime(signUpRequest.getMealTime())
+                .id(savedUser.getId())
+                .build();
+
+        userMealTimeRepository.save(userMealTime);
+
+//        SignUpResponse response = jwtTokenProvider.createAllToken(user.getEmail());
+
+//        return response;
+        return "OK";
+    }
+
+    public SignInResponse signIn(SignInRequest signInRequest) {
+
+        User user = userRepository.findByEmail(signInRequest.getEmail()).orElseThrow(
+                () -> new RuntimeException("Not found Account")
+        );
+
+        if(!passwordEncoder.matches(signInRequest.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Not matches Password");
+        }
+
+        SignInResponse tokenDto = jwtTokenProvider.createAllToken(signInRequest.getEmail());
+
+        Optional<RefreshToken> refreshToken = refreshTokenRepository.findByEmail(signInRequest.getEmail());
+
+        if(refreshToken.isPresent()) {
+            refreshTokenRepository.save(refreshToken.get().updateToken(tokenDto.getRefreshToken()));
+        }else {
+            RefreshToken newToken = new RefreshToken(tokenDto.getRefreshToken(), signInRequest.getEmail());
+            refreshTokenRepository.save(newToken);
+        }
+
+        return tokenDto;
+    }
+}
