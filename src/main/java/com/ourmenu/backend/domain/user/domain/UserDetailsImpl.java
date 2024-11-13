ad7b2b002e6731178c4c829c4c78d9a2904a02ff
You-Hyuk <cxz6411@naver.com>
2024-11-13 13:38:18 +0900

    fix: UserDetails 오버라이드 메소드 삭제 및 수정

--- src/main/java/com/ourmenu/backend/domain/user/domain/UserDetailsImpl.java
+++ src/main/java/com/ourmenu/backend/domain/user/domain/UserDetailsImpl.java
@@ -2,9 +2,11 @@ package com.ourmenu.backend.domain.user.domain;
 
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
 @Data
 public class UserDetailsImpl implements UserDetails {
@@ -12,37 +14,17 @@ public class UserDetailsImpl implements UserDetails {
     private User user;
 
     @Override
-    public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
-    }
-
-    @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getEmail();
     }
 }
