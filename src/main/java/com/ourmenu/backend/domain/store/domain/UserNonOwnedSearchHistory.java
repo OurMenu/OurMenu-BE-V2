7f94eb2488a29057962e14c7709fdecbffad44da
david-parkk <ji0513ji@naver.com>
2024-11-18 15:13:37 +0900

    feat: 검색 기록 엔티티 추가
    - UserOwnedSearchHistory 엔티티(유저가 소유한 가게 검색)
    - UserNonOwnedSearchHistory 엔티티(유저가 소유하지 않은 가게 검색)

---
+++ src/main/java/com/ourmenu/backend/domain/store/domain/UserNonOwnedSearchHistory.java
@@ -0,0 +1,36 @@
+package com.ourmenu.backend.domain.store.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_non_owned_search_history")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+public class UserNonOwnedSearchHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private LocalDateTime search_at;
+
+    @NotNull
+    private Long userId;
+}
