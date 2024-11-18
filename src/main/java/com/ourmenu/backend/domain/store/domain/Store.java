04d15b40f79d454b39bca4b4864b0d345f8ef178
david-parkk <ji0513ji@naver.com>
2024-11-18 14:36:38 +0900

    feat(Store,Map): 식당 관련 엔티티 추가
    - Store 엔티티
    - Map 엔티티

---
+++ src/main/java/com/ourmenu/backend/domain/store/domain/Store.java
@@ -0,0 +1,29 @@
+package com.ourmenu.backend.domain.store.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String title;
+
+    private String address;
+}
