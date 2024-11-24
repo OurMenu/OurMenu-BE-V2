de0b558b4c8daebd21f5011bf5da4e2bb4f1cb66
You-Hyuk <cxz6411@naver.com>
2024-11-24 19:54:07 +0900

    feat: ConfirmCode Builder 추가 및 메소드 구현

--- src/main/java/com/ourmenu/backend/domain/user/domain/ConfirmCode.java
+++ src/main/java/com/ourmenu/backend/domain/user/domain/ConfirmCode.java
@@ -3,13 +3,17 @@ package com.ourmenu.backend.domain.user.domain;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @RedisHash(value = "confirmCode", timeToLive = 5 * 60)
+@Builder
 public class ConfirmCode {
 
     @Id
@@ -19,4 +23,11 @@ public class ConfirmCode {
     private String email;
 
     private String confirmCode;
+
+    public static ConfirmCode of(String email, String confirmCode){
+        return ConfirmCode.builder()
+                .email(email)
+                .confirmCode(confirmCode)
+                .build();
+    }
 }
