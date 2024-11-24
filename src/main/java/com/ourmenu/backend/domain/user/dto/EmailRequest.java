e86dfd62048eb6e8954544e39bf928edb3736641
You-Hyuk <cxz6411@naver.com>
2024-11-24 18:19:18 +0900

    feat: DTO 생성자 어노테이션 추가

--- src/main/java/com/ourmenu/backend/domain/user/dto/EmailRequest.java
+++ src/main/java/com/ourmenu/backend/domain/user/dto/EmailRequest.java
@@ -1,10 +1,14 @@
 package com.ourmenu.backend.domain.user.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmailRequest {
     private String email;
 }
