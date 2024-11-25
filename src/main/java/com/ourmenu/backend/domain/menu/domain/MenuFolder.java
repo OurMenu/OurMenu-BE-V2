0a895d22607dbf8f0214eb9d228be27cb5f45f42
david-parkk <ji0513ji@naver.com>
2024-11-25 10:03:39 +0900

    fix(MenuFolder): 연관관계 추가

--- src/main/java/com/ourmenu/backend/domain/menu/domain/MenuFolder.java
+++ src/main/java/com/ourmenu/backend/domain/menu/domain/MenuFolder.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -23,7 +24,8 @@ public class MenuFolder {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long menuId;
+    @ManyToOne
+    private Menu menu;
 
     private Long folderId;
 }
