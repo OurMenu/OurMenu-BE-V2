b9d1316f8a717e2ae20f42aba505e66ffbcb8fcc
You-Hyuk <cxz6411@naver.com>
2024-12-24 13:53:14 +0900

    feat: Validation Exception 처리 및 에러코드 수정

--- src/main/java/com/ourmenu/backend/global/exception/ErrorCode.java
+++ src/main/java/com/ourmenu/backend/global/exception/ErrorCode.java
@@ -10,10 +10,12 @@ public enum ErrorCode {
 
     INTERNAL_SERVER(HttpStatus.INTERNAL_SERVER_ERROR, "G500", "서버 내부에서 에러가 발생하였습니다"),
 
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "G400", "잘못된 요청입니다."),
+
     // 유저
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U500", "해당 유저가 존재하지 않습니다."),
-    PASSWORD_NOT_MATCH(HttpStatus.UNAUTHORIZED, "U501", "비밀번호가 일치하지 않습니다."),
-    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "U502", "이미 존재하는 이메일입니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U404", "해당 유저가 존재하지 않습니다."),
+    PASSWORD_NOT_MATCH(HttpStatus.UNAUTHORIZED, "U401", "비밀번호가 일치하지 않습니다."),
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT, "U409", "이미 존재하는 이메일입니다.");
 
     private final HttpStatus httpStatus;
 
