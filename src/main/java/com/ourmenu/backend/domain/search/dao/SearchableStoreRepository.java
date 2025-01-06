18c0759a149293463f0fd52ec2dbb7d0876d8a4d
david-parkk <ji0513ji@naver.com>
2025-01-06 15:12:07 +0900

    fix: store 엔티티 수정
    - 테스트용 dao 메서드 생성

--- src/main/java/com/ourmenu/backend/domain/search/dao/SearchableStoreRepository.java
+++ src/main/java/com/ourmenu/backend/domain/search/dao/SearchableStoreRepository.java
@@ -1,12 +1,12 @@
 package com.ourmenu.backend.domain.search.dao;
 
 import com.ourmenu.backend.domain.search.domain.SearchableStore;
-import java.util.Optional;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SearchableStoreRepository extends MongoRepository<SearchableStore, String> {
 
-    Optional<SearchableStore> findByNameContaining(String name);
+    List<SearchableStore> findAllByNameContaining(String name);
 }
