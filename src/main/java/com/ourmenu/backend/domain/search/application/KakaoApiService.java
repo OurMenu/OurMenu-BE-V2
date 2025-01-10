97d67d29430f33fc8fb6c3fa71b7ab1c5c7b00b3
david-parkk <ji0513ji@naver.com>
2025-01-10 13:31:03 +0900

    feat: 카카오 검색 로직 예외 추가
    - 1. 카카오 API 사용량 초과시 예외
    - 2. 검색 결과 없을시 예외
    - 리뷰 반영

--- src/main/java/com/ourmenu/backend/domain/search/application/KakaoApiService.java
+++ src/main/java/com/ourmenu/backend/domain/search/application/KakaoApiService.java
@@ -3,8 +3,11 @@ package com.ourmenu.backend.domain.search.application;
 import com.ourmenu.backend.domain.search.domain.NotFoundStore;
 import com.ourmenu.backend.domain.search.dto.KaKaoMapDto;
 import com.ourmenu.backend.domain.search.dto.KakaoAPIResponse;
+import com.ourmenu.backend.domain.search.exception.ExceededDailyQuotaException;
+import com.ourmenu.backend.domain.search.exception.NotFoundStoreInKakaoException;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -23,17 +26,13 @@ public class KakaoApiService {
                 .defaultHeader("Authorization", kakaoAPIKey)
                 .build();
     }
-
+    
     public NotFoundStore searchKakaoMap(String query) {
-        KakaoAPIResponse response = webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .queryParam("query", query)
-                        .queryParam("size", 1)
-                        .build()
-                )
-                .retrieve()
-                .bodyToMono(KakaoAPIResponse.class)
-                .block();
+        KakaoAPIResponse response = getKakaoAPI(query);
+
+        if (response.getDocuments().length == 0) {
+            throw new NotFoundStoreInKakaoException();
+        }
         KaKaoMapDto kaKaoMapDto = response.getDocuments()[0];
         return NotFoundStore.builder()
                 .title(kaKaoMapDto.getPlaceName())
@@ -42,5 +41,23 @@ public class KakaoApiService {
                 .mapY(kaKaoMapDto.getMapY())
                 .storeId(kaKaoMapDto.getStoreId())
                 .build();
+    }
+
+    private KakaoAPIResponse getKakaoAPI(String query) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("query", query)
+                        .queryParam("size", 1)
+                        .build()
+                )
+                .retrieve()
+                .onStatus(
+                        //http code 확인(일일 사용량 초과시 예외)
+                        HttpStatus.UNAUTHORIZED::equals,
+                        kakaoResponse -> kakaoResponse.bodyToMono(String.class)
+                                .map(errorBody -> new ExceededDailyQuotaException())
+                )
+                .bodyToMono(KakaoAPIResponse.class)
+                .block();
     }
 }
