b7f0bd30c81fb11a97fb6ba88b1310f75c967058
david-parkk <ji0513ji@naver.com>
2025-01-20 00:43:01 +0900

    fix: CD workflow

--- infra/script/deploy.sh
+++ infra/script/deploy.sh
@@ -6,8 +6,8 @@ docker-compose -f infra/docker/docker-compose.yml down --rmi local --volumes
 echo "최신 버전 pull"
 git pull origin main
 
-echo "서버 빌드"
-docker-compose -f infra/docker/docker-compose.yml build --no-cache
+#echo "서버 빌드"
+#docker-compose -f infra/docker/docker-compose.yml build --no-cache
 
 echo "서버 실행"
 docker-compose -f infra/docker/docker-compose.yml up --build -d
