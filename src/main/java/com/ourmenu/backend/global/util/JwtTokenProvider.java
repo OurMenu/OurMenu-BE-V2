f3ae558bd8ae486f56168545332552f352edca2e
david-parkk <ji0513ji@naver.com>
2025-02-03 16:28:14 +0900

    fix: JWT EXP
    - 1분 -> 1시간

--- src/main/java/com/ourmenu/backend/global/util/JwtTokenProvider.java
+++ src/main/java/com/ourmenu/backend/global/util/JwtTokenProvider.java
@@ -6,11 +6,22 @@ import com.ourmenu.backend.domain.user.domain.RefreshToken;
 import com.ourmenu.backend.domain.user.dto.response.TokenDto;
 import com.ourmenu.backend.domain.user.exception.InvalidTokenException;
 import com.ourmenu.backend.domain.user.exception.TokenExpiredExcpetion;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,13 +30,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-
-import java.security.Key;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Base64;
-import java.util.Date;
-import java.util.Optional;
 
 @Component
 @Slf4j
@@ -39,8 +43,8 @@ public class JwtTokenProvider {
     private Key key;
     private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
-    private static final long ACCESS_TIME =  60 * 1000L;   // 1시간
-    private static final long REFRESH_TIME =  30 * 24 * 60 * 60 * 1000L;    // 30일
+    private static final long ACCESS_TIME = 60 * 60 * 1000L;   // 1시간
+    private static final long REFRESH_TIME = 30 * 24 * 60 * 60 * 1000L;    // 30일
     public static final String ACCESS_TOKEN = "Authorization";
     public static final String REFRESH_TOKEN = "Refresh-Token";
 
@@ -49,8 +53,8 @@ public class JwtTokenProvider {
 
     /**
      * 환경 변수로 지정한 SecretKey값으로 JWT 서명 키를 초기화
-     * @PostConstruct 어노테이션에 의해 의존성 주입이 완료된 후 자동으로 실행
-     * base64로 인코딩된 비밀 키를 디코딩, JWT 토큰 생성 및 검증에 사용할 서명 키를 생성
+     *
+     * @PostConstruct 어노테이션에 의해 의존성 주입이 완료된 후 자동으로 실행 base64로 인코딩된 비밀 키를 디코딩, JWT 토큰 생성 및 검증에 사용할 서명 키를 생성
      */
     @PostConstruct
     public void init() {
@@ -60,8 +64,9 @@ public class JwtTokenProvider {
 
     /**
      * Request로부터 입력받은 Type에 해당하는 Token값을 반환
+     *
      * @param request 사용자의 HTTP Request
-     * @param type Token의 종류
+     * @param type    Token의 종류
      * @return AccessToken값 혹은 RefreshToken값
      */
     public String getHeaderToken(HttpServletRequest request, String type) {
@@ -80,6 +85,7 @@ public class JwtTokenProvider {
 
     /**
      * User의 email을 입력받아 AccessToken 및 RefreshToken 생성 및 해당 정보 반환
+     *
      * @param email User의 Email
      * @return JWT 정보를 DTO로 반환
      */
@@ -97,8 +103,9 @@ public class JwtTokenProvider {
 
     /**
      * 해당 Email에 입력받은 Type의 JWT Token을 생성
+     *
      * @param email User의 Email
-     * @param type Token의 종류
+     * @param type  Token의 종류
      * @return 생성한 Token값
      */
     public String createToken(String email, String type) {
@@ -110,7 +117,7 @@ public class JwtTokenProvider {
 
         long time = ACCESS_TIME;
 
-        if (type.equals("Refresh")){
+        if (type.equals("Refresh")) {
             time = REFRESH_TIME;
         }
 
@@ -124,6 +131,7 @@ public class JwtTokenProvider {
 
     /**
      * 해당 토큰이 유효한지 검증
+     *
      * @param token JWT 토큰값
      * @return Token의 유효 여부(True, False)
      */
@@ -131,20 +139,21 @@ public class JwtTokenProvider {
         try {
             Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
             return true;
-        } catch (ExpiredJwtException e){
+        } catch (ExpiredJwtException e) {
             throw new TokenExpiredExcpetion();
-        } catch (JwtException e){
+        } catch (JwtException e) {
             throw new InvalidTokenException();
         }
     }
 
     /**
      * 해당 RefreshToken이 유효한지 1차 검증 및 Redis에 저장된 RefreshToken과 동일한지 2차 검증
+     *
      * @param token RefreshToken값
      * @return RefreshToken 유효 여부 (True, False)
      */
     public Boolean refreshTokenValidation(String token) {
-        if(!validateToken(token)) {
+        if (!validateToken(token)) {
             return false;
         }
 
@@ -154,6 +163,7 @@ public class JwtTokenProvider {
 
     /**
      * 해당 Email로부터 UserDetails객체를 불러온 이후 인증 객체 생성
+     *
      * @param email User의 Email
      * @return 인증 객체(Authentication)
      */
@@ -164,16 +174,19 @@ public class JwtTokenProvider {
 
     /**
      * Token값으로부터 해당 User의 Email정보을 추출
+     *
      * @param token Token값 (Access)
      * @return Email
      */
     public String getEmailFromToken(String token) {
-        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().get("email", String.class);
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody()
+                .get("email", String.class);
     }
 
     /**
      * Response의 Header에 AccessToken값을 설정
-     * @param response HTTP Response
+     *
+     * @param response    HTTP Response
      * @param accessToken AccessToken값
      */
     public void setHeaderAccessToken(HttpServletResponse response, String accessToken) {
@@ -182,7 +195,8 @@ public class JwtTokenProvider {
 
     /**
      * Response의 Header에 RefreshToken값을 설정
-     * @param response HTTP Response
+     *
+     * @param response     HTTP Response
      * @param refreshToken RefreshToken값
      */
     public void setHeaderRefreshToken(HttpServletResponse response, String refreshToken) {
@@ -191,6 +205,7 @@ public class JwtTokenProvider {
 
     /**
      * 토큰의 만료 시간을 반환하는 메서드
+     *
      * @param token JWT 토큰 값
      * @return 만료 시간 (Date)
      */
