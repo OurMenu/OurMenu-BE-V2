3ddc858a11e8a80c505c9e694a27f5ba1ff6bd4e
You-Hyuk <cxz6411@naver.com>
2025-02-08 02:23:45 +0900

    fix: CustomOAuth2UserSuccessHandler 로직 변경

--- src/main/java/com/ourmenu/backend/domain/user/handler/CustomOAuth2UserSuccessHandler.java
+++ src/main/java/com/ourmenu/backend/domain/user/handler/CustomOAuth2UserSuccessHandler.java
@@ -1,10 +1,13 @@
 package com.ourmenu.backend.domain.user.handler;
 
 import com.ourmenu.backend.domain.user.dao.RefreshTokenRepository;
+import com.ourmenu.backend.domain.user.dao.UserRepository;
 import com.ourmenu.backend.domain.user.domain.RefreshToken;
+import com.ourmenu.backend.domain.user.domain.SignInType;
+import com.ourmenu.backend.domain.user.domain.User;
 import com.ourmenu.backend.domain.user.dto.response.TokenDto;
+import com.ourmenu.backend.global.response.util.ApiUtil;
 import com.ourmenu.backend.global.util.JwtTokenProvider;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +16,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -25,42 +27,41 @@ public class CustomOAuth2UserSuccessHandler extends SimpleUrlAuthenticationSucce
 
     private final JwtTokenProvider jwtTokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
 
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         String email = oAuth2User.getAttribute("email");
         String provider = oAuth2User.getAttribute("provider");
+        String accessToken = extractAccessToken(request);
 
-        boolean isExist = Boolean.TRUE.equals(oAuth2User.getAttribute("exist"));
-        String role = oAuth2User.getAuthorities().stream().
-                findFirst()
-                .orElseThrow(IllegalAccessError::new)
-                .getAuthority();
-
-        if (isExist) {
-            TokenDto tokenDto = jwtTokenProvider.createAllToken(email);
-            String accessToken = tokenDto.getAccessToken();
-            RefreshToken refreshToken = new RefreshToken(tokenDto.getRefreshToken(), email);
-            refreshTokenRepository.save(refreshToken);
-
-            String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:8080/loginSuccess")
-                    .queryParam("accessToken", accessToken)
-                    .build()
-                    .encode(StandardCharsets.UTF_8)
-                    .toUriString();
-           getRedirectStrategy().sendRedirect(request, response, targetUrl);
-           return;
+        if (email == null || accessToken == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "필수 정보가 없습니다.");
+            return;
         }
 
-        String targetUrl = UriComponentsBuilder.fromUriString("http://localhost:8080/loginSuccess")
-                .queryParam("email", (String) oAuth2User.getAttribute("email"))
-                .queryParam("provider", provider)
-                .build()
-                .encode(StandardCharsets.UTF_8)
-                .toUriString();
-        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+        Optional<User> existingUser = userRepository.findByEmail(email);
+        User user = existingUser.orElseGet(() -> registerKakaoUser(email));
+
+        TokenDto tokenDto = jwtTokenProvider.createAllToken(user.getEmail());
+        RefreshToken refreshToken = new RefreshToken(tokenDto.getRefreshToken(), user.getEmail());
+
+        refreshTokenRepository.save(refreshToken);
+
+        ApiUtil.sendSuccessResponse(response, ApiUtil.success(tokenDto));
     }
 
+    private String extractAccessToken(HttpServletRequest request) {
+        return request.getParameter("code");
+    }
+
+    private User registerKakaoUser(String email) {
+        User newUser = User.builder()
+                .email(email)
+                .signInType(SignInType.KAKAO)
+                .build();
+        return userRepository.save(newUser);
+    }
 }
