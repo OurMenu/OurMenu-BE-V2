f7fc06f1a69f4bfe7ad527d452f357cf8f6b8ace
david-parkk <ji0513ji@naver.com>
2025-04-10 17:45:17 +0900

    test: 테스트 추가
    - 메뉴판_정보_및_메뉴판에_속한_메뉴들을_조회_할_수_있다

--- src/test/java/com/ourmenu/backend/domain/menu/data/MenuTestData.java
+++ src/test/java/com/ourmenu/backend/domain/menu/data/MenuTestData.java
@@ -4,6 +4,8 @@ import com.ourmenu.backend.domain.cache.domain.MenuFolderIcon;
 import com.ourmenu.backend.domain.cache.domain.MenuPin;
 import com.ourmenu.backend.domain.menu.domain.Menu;
 import com.ourmenu.backend.domain.menu.domain.MenuFolder;
+import com.ourmenu.backend.domain.menu.domain.MenuMenuFolder;
+import com.ourmenu.backend.domain.store.domain.Store;
 import com.ourmenu.backend.domain.user.domain.CustomUserDetails;
 import jakarta.persistence.EntityManager;
 import java.util.List;
@@ -12,9 +14,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class MenuTestData {
 
     private EntityManager entityManager;
+    private StoreTestData storeTestData;
 
-    public MenuTestData(EntityManager entityManager) {
+    public MenuTestData(EntityManager entityManager, StoreTestData storeTestData) {
         this.entityManager = entityManager;
+        this.storeTestData = storeTestData;
     }
 
     @Transactional
@@ -73,5 +77,67 @@ public class MenuTestData {
 
         entityManager.persist(menu);
         return menu;
+    }
+
+    @Transactional
+    public List<Menu> createTestMenusWithStore(CustomUserDetails customUserDetails) {
+        Store store = storeTestData.createTestStore();
+
+        Menu menu1 = Menu.builder()
+                .title("테스트 메뉴1 비비큐")
+                .price(1000)
+                .pin(MenuPin.BBQ)
+                .memoTitle("테스트 메뉴1 메모 제목")
+                .memoContent("테스트 메뉴1 메모 내용")
+                .store(store)
+                .isCrawled(Boolean.FALSE)
+                .userId(customUserDetails.getId())
+                .build();
+
+        Menu menu2 = Menu.builder()
+                .title("테스트 메뉴2 빵")
+                .price(2000)
+                .pin(MenuPin.BREAD)
+                .memoTitle("테스트 메뉴2 메모 제목")
+                .memoContent("테스트 메뉴2 메모 내용")
+                .store(store)
+                .isCrawled(Boolean.FALSE)
+                .userId(customUserDetails.getId())
+                .build();
+
+        Menu menu3 = Menu.builder()
+                .title("테스트 메뉴3 케익")
+                .price(3000)
+                .pin(MenuPin.CAKE)
+                .memoTitle("테스트 메뉴3 메모 제목")
+                .memoContent("테스트 메뉴3 메모 내용")
+                .store(store)
+                .isCrawled(Boolean.FALSE)
+                .userId(customUserDetails.getId())
+                .build();
+
+        entityManager.persist(menu1);
+        entityManager.persist(menu2);
+        entityManager.persist(menu3);
+        return List.of(menu1, menu2, menu3);
+    }
+
+    /**
+     * 메뉴판 생성 및 메뉴들과 연결합니다.
+     *
+     * @param customUserDetails
+     * @return
+     */
+    @Transactional
+    public MenuFolder createMenuFolderWithMenu(CustomUserDetails customUserDetails) {
+        MenuFolder menuFolder = createTestMenuFolder(customUserDetails);
+        List<Menu> menus = createTestMenusWithStore(customUserDetails);
+        menus.stream()
+                .map(menu -> MenuMenuFolder.builder()
+                        .menu(menu)
+                        .folderId(menuFolder.getId())
+                        .build())
+                .forEach(entityManager::persist);
+        return menuFolder;
     }
 }
