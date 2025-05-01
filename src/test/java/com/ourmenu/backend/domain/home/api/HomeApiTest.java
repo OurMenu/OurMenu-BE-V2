266f1dc01f101861bfe5dea01be69e41bd08ec42
david-parkk <ji0513ji@naver.com>
2025-05-01 17:55:09 +0900

    test: 테스트 추가
    - 온보딩_질문_응답에_대한_추천_메뉴를_구성한다

--- src/test/java/com/ourmenu/backend/domain/home/api/HomeApiTest.java
+++ src/test/java/com/ourmenu/backend/domain/home/api/HomeApiTest.java
@@ -4,6 +4,7 @@ import com.ourmenu.backend.domain.home.config.HomeTestConfig;
 import com.ourmenu.backend.domain.home.data.HomeTestData;
 import com.ourmenu.backend.domain.home.data.UserTestData;
 import com.ourmenu.backend.domain.home.domain.Answer;
+import com.ourmenu.backend.domain.home.dto.GetHomeRecommendResponse;
 import com.ourmenu.backend.domain.home.dto.SaveAndGetQuestionRequest;
 import com.ourmenu.backend.domain.home.dto.SaveAnswerRequest;
 import com.ourmenu.backend.domain.user.domain.CustomUserDetails;
@@ -58,7 +59,7 @@ public class HomeApiTest {
     @Test
     void 온보딩_질문_답을_저장_할_수_있다() {
         //given
-        homeTestData.createQuestion(testCustomUserDetails.getId());
+        homeTestData.createTestQuestion(testCustomUserDetails.getId());
 
         //when
         SaveAnswerRequest request = new SaveAnswerRequest(Answer.LIKE);
@@ -71,7 +72,7 @@ public class HomeApiTest {
     @Test
     void 올바르지_않는_온보딩_질문_답에_대해서_예외를_발생시킨다() {
         //given
-        homeTestData.createQuestion(testCustomUserDetails.getId());
+        homeTestData.createTestQuestion(testCustomUserDetails.getId());
 
         //when
         SaveAnswerRequest request = new SaveAnswerRequest(Answer.SEA);
@@ -79,6 +80,22 @@ public class HomeApiTest {
         //then
         Assertions.assertThatThrownBy(() -> homeController.saveQuestionAnswer(request, testCustomUserDetails))
                 .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void 온보딩_질문_응답에_대한_추천_메뉴를_구성한다() {
+        //given
+        homeTestData.createTestHomeMenu(testCustomUserDetails);
+
+        //when
+        SaveAnswerRequest request = new SaveAnswerRequest(Answer.LIKE);
+        homeController.saveQuestionAnswer(request, testCustomUserDetails);
+        ApiResponse<GetHomeRecommendResponse> response = homeController.getHomeMenus(testCustomUserDetails);
+
+        //then
+        Assertions.assertThat(response.isSuccess()).isEqualTo(true);
+        Assertions.assertThat(response.getResponse().getAnswer()).isEqualTo(Answer.LIKE);
+        Assertions.assertThat(response.getResponse().getAnswerRecommendMenus().size()).isEqualTo(2);
     }
 
 }
--- src/test/java/com/ourmenu/backend/domain/home/data/HomeTestData.java
+++ src/test/java/com/ourmenu/backend/domain/home/data/HomeTestData.java
@@ -1,8 +1,19 @@
 package com.ourmenu.backend.domain.home.data;
 
+import com.ourmenu.backend.domain.cache.domain.MenuPin;
 import com.ourmenu.backend.domain.home.domain.HomeQuestionAnswer;
 import com.ourmenu.backend.domain.home.domain.Question;
+import com.ourmenu.backend.domain.menu.domain.Menu;
+import com.ourmenu.backend.domain.store.domain.Map;
+import com.ourmenu.backend.domain.store.domain.Store;
+import com.ourmenu.backend.domain.tag.domain.MenuTag;
+import com.ourmenu.backend.domain.tag.domain.Tag;
+import com.ourmenu.backend.domain.user.domain.CustomUserDetails;
 import jakarta.persistence.EntityManager;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.PrecisionModel;
 import org.springframework.transaction.annotation.Transactional;
 
 public class HomeTestData {
@@ -14,12 +25,82 @@ public class HomeTestData {
     }
 
     @Transactional
-    public HomeQuestionAnswer createQuestion(Long userId) {
+    public HomeQuestionAnswer createTestQuestion(Long userId) {
         HomeQuestionAnswer homeQuestionAnswer = HomeQuestionAnswer.builder()
                 .question(Question.FEEL)
                 .userId(userId)
                 .build();
         entityManager.persist(homeQuestionAnswer);
         return homeQuestionAnswer;
+    }
+
+    /**
+     * 홈 화면에 필요한 테스트 데이터를 저장한다
+     *
+     * @param userId
+     */
+    @Transactional
+    public void createTestHomeMenu(CustomUserDetails customUserDetails) {
+        int srid = 4326;
+        GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(), srid);
+
+        Coordinate coordinate = new Coordinate(127.085, 37.538);
+        Point location = geometryFactory.createPoint(coordinate);
+
+        Map map = Map.builder()
+                .location(location)
+                .build();
+        entityManager.persist(map);
+
+        Store store = Store.builder()
+                .title("테스트 식당1")
+                .address("광진구 어딘가")
+                .map(map)
+                .build();
+        entityManager.persist(store);
+
+        Menu menu1 = Menu.builder()
+                .title("테스트 메뉴1 비비큐")
+                .price(1000)
+                .pin(MenuPin.BBQ)
+                .memoTitle("테스트 메뉴1 메모 제목")
+                .memoContent("테스트 메뉴1 메모 내용")
+                .store(store)
+                .isCrawled(Boolean.FALSE)
+                .userId(customUserDetails.getId())
+                .build();
+
+        Menu menu2 = Menu.builder()
+                .title("테스트 메뉴2 빵")
+                .price(2000)
+                .pin(MenuPin.BREAD)
+                .memoTitle("테스트 메뉴2 메모 제목")
+                .memoContent("테스트 메뉴2 메모 내용")
+                .store(store)
+                .isCrawled(Boolean.FALSE)
+                .userId(customUserDetails.getId())
+                .build();
+
+        entityManager.persist(menu1);
+        entityManager.persist(menu2);
+
+        MenuTag menuTag1 = MenuTag.builder()
+                .menuId(menu1.getId())
+                .tag(Tag.PROMISE)
+                .build();
+
+        MenuTag menuTag2 = MenuTag.builder()
+                .menuId(menu2.getId())
+                .tag(Tag.PROMISE)
+                .build();
+
+        entityManager.persist(menuTag1);
+        entityManager.persist(menuTag2);
+
+        HomeQuestionAnswer homeQuestionAnswer = HomeQuestionAnswer.builder()
+                .question(Question.FEEL)
+                .userId(customUserDetails.getId())
+                .build();
+        entityManager.persist(homeQuestionAnswer);
     }
 }
