97b0a92d90a529d4152ceccbb8f4058b2bd88417
You-Hyuk <cxz6411@naver.com>
2025-05-12 16:34:04 +0900

    test: 이메일 인증 코드 검증 API 테스트

--- src/test/java/com/ourmenu/backend/domain/user/api/EmailApiTest.java
+++ src/test/java/com/ourmenu/backend/domain/user/api/EmailApiTest.java
@@ -1,6 +1,7 @@
 package com.ourmenu.backend.domain.user.api;
 
 import com.ourmenu.backend.domain.user.dto.request.PostEmailRequest;
+import com.ourmenu.backend.domain.user.dto.request.VerifyEmailRequest;
 import com.ourmenu.backend.domain.user.dto.response.EmailResponse;
 import com.ourmenu.backend.domain.user.dto.response.TemporaryPasswordResponse;
 import com.ourmenu.backend.global.DatabaseCleaner;
@@ -38,6 +39,21 @@ public class EmailApiTest {
 
         //when
         ApiResponse<EmailResponse> response = emailController.sendConfirmCode(request);
+
+        //then
+        Assertions.assertThat(response.isSuccess()).isEqualTo(true);
+    }
+    
+    @Test
+    public void 이메일_인증_코드를_검증한다() {
+        //given
+        PostEmailRequest emailRequest = new PostEmailRequest("test123@naver.com");
+        ApiResponse<EmailResponse> emailResponse = emailController.sendConfirmCode(emailRequest);
+        VerifyEmailRequest request = new VerifyEmailRequest("test123@naver.com",
+                emailResponse.getResponse().getCode());
+
+        //when
+        ApiResponse<Void> response = emailController.verifyEmail(request);
 
         //then
         Assertions.assertThat(response.isSuccess()).isEqualTo(true);
