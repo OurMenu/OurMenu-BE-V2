6832b0c2b75d8827f9f70790882d4692e9ef52df
You-Hyuk <cxz6411@naver.com>
2025-05-15 18:07:02 +0900

    fix: SignUpRequest mealTime 자료형 변경

--- src/main/java/com/ourmenu/backend/domain/user/dto/request/SignUpRequest.java
+++ src/main/java/com/ourmenu/backend/domain/user/dto/request/SignUpRequest.java
@@ -3,13 +3,13 @@ package com.ourmenu.backend.domain.user.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class SignUpRequest {
     private String email;
     private String password;
-    private ArrayList<Integer> mealTime;
+    private List<Integer> mealTime;
     private String signInType;
 }
