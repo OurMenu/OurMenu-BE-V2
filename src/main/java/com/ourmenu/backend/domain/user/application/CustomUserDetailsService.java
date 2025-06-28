8a3b54cec1e7718bf4efd13e905e75ff7b79b076
You-Hyuk <cxz6411@naver.com>
2025-06-28 20:42:17 +0900

    fix: UserDetailService 제거

--- src/main/java/com/ourmenu/backend/domain/user/application/CustomUserDetailsService.java
+++ src/main/java/com/ourmenu/backend/domain/user/application/CustomUserDetailsService.java
@@ -8,29 +8,15 @@ import com.ourmenu.backend.domain.user.exception.NotFoundUserException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService {
 
     private final UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-
-        User user= userRepository.findByEmailAndSignInType(email, SignInType.EMAIL).
-                orElseThrow(NotFoundUserException::new);
-
-        return new CustomUserDetails(
-                user.getId(),
-                user.getEmail(),
-                user.getPassword()
-        );
-    }
 
     public UserDetails loadUserByEmailAndSignInType(String email, SignInType signInType) throws UsernameNotFoundException {
         User user= userRepository.findByEmailAndSignInType(email, signInType).
